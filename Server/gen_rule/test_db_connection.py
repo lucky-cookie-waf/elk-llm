b6db592b73e8365c37a7e3612472099ffe908b4f
aroro6210 <aroro6210@ewha.ac.kr>
2025-12-02 15:45:09 +0900

    db연동수정

--- Server/gen_rule/test_db_connection.py
+++ Server/gen_rule/test_db_connection.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.nqpshpimhofnjxlcepop:luckycookiedb123@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?sslmode=require&schema=public&pgbouncer=true&connection_limit=1")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.cykbxbwpvkomvqqdbdrc:luckycookie2233!!@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true")
 
 def test_db_connection():
     """데이터베이스 연결을 테스트합니다."""
